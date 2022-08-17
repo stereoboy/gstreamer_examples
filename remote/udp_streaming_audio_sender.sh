4ba7fcd27edd936aa0e4cc19fa73f5a2c83b1655
Jaeyoung Lee <jylee0121@gmail.com>
2022-08-17 16:03:19 +0900

    update: update udp streaming audio sender
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/udp_streaming_audio_sender.sh
+++ remote/udp_streaming_audio_sender.sh
@@ -1,40 +1,75 @@
 #!/bin/sh
 
-# work, low-quality, little slow
-#gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
-#  alawenc ! rtppcmapay !application/x-rtp, payload=8, clock-rate=8000 !\
-#  udpsink host=127.0.0.1 port=8555
+HOST=192.168.0.4
 
-# work, high-quality, little slow
-gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
-  alawenc ! rtpgstpay !\
-  udpsink host=127.0.0.1 port=8555
+set -x
 
-# runnable, not work well, very slow
-#gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
-#  "audio/x-raw, format=(string)U8" ! rtpL8pay ! "application/x-rtp, media=audio, payload=96, clock-rate=90000, encoding-name=L8, channel=2" !\
-#  udpsink host=127.0.0.1 port=8555
+echo "target is $@"
 
-# work, slow
-#gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
-#  avenc_ac3 ! rtpac3pay ! \
-#  udpsink host=127.0.0.1 port=8555
+case $@ in
 
-#########################################################################
-#
-# references https://marc.info/?l=gstreamer-devel&m=150650921723890&w=2
-# work, slow
-#gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
-#  avenc_aac ! rtpmp4apay ! \
-#  udpsink host=127.0.0.1 port=8555
+    0)
+        echo "Alaw codec clock-rate=8000"
+        # working, low-quality, little slow
+        gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+          alawenc ! rtppcmapay !application/x-rtp, payload=8, clock-rate=8000 !\
+          udpsink host=$HOST port=8555
+        ;;
+    1)
+        echo "Alaw codec clock-rate=900000"
+        # working, high-quality, little slow
+        gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+          alawenc ! rtpgstpay !\
+          udpsink host=$HOST port=8555
 
-#gst-launch-1.0 -v audiotestsrc ! audioconvert ! avenc_aac ! rtpmp4apay ! \
-#  udpsink host=127.0.0.1 port=8555
+        ;;
+    2)
+        echo "Raw Audio L8"
+        # working, noisy, little slow
+        gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+          rtpL8pay ! \
+          udpsink host=$HOST port=8555
+        ;;
+    3)
+        echo "Raw Audio L16"
+        # working, little slow
+        gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+          rtpL16pay !\
+          udpsink host=$HOST port=8555
+        ;;
+    4)
+        echo "A3C codec"
+        # work, slow
+        gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+          avenc_ac3 ! rtpac3pay ! \
+          udpsink host=$HOST port=8555
+        ;;
 
-#gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
-#  avenc_aac ! avdec_aac! \
-#  autoaudiosink
-#########################################################################
+    5)
+        echo "AAC codec"
+        #########################################################################
+        #
+        # references https://marc.info/?l=gstreamer-devel&m=150650921723890&w=2
+        # work, slow
+        gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+          avenc_aac ! rtpmp4apay ! \
+          udpsink host=$HOST port=8555
+
+        #gst-launch-1.0 -v audiotestsrc ! audioconvert ! avenc_aac ! rtpmp4apay ! \
+        #  udpsink host=127.0.0.1 port=8555
+
+        #gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\
+        #  avenc_aac ! avdec_aac! \
+        #  autoaudiosink
+        #########################################################################
+        ;;
+
+esac
+
+
+
+
+
 
 #########################################################################
 #gst-launch-1.0 -v pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo !\

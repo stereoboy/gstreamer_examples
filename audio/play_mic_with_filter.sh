48797184853c2516fd8af6a6197adc0b8f75d3bf
Jaeyoung Lee <jylee0121@gmail.com>
2023-07-25 20:31:34 +0900

    fix: fix AUDIO_SRC
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- audio/play_mic_with_filter.sh
+++ audio/play_mic_with_filter.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -x
 
-AUDIO_SRC=pulsesrc device=alsa_input.usb-046d_HD_Pro_Webcam_C920_9D5E927F-02.analog-stereo
+AUDIO_SRC="pulsesrc device=alsa_input.usb-046d_HD_Pro_Webcam_C920_9D5E927F-02.analog-stereo"
+#AUDIO_SRC="pulsesrc device=alsa_input.usb-046d_Logitech_StreamCam_6A86D645-02.analog-stereo"
 
 gst-launch-1.0 -v $AUDIO_SRC ! audioconvert ! audioresample ! \
     webrtcdsp ! webrtcechoprobe ! \

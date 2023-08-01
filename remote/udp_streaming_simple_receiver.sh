eb5ebc5d6d6eb309ef16e43f3168439192472d07
Jaeyoung Lee <jylee0121@gmail.com>
2023-08-01 15:16:33 +0900

    update: update udp_streaming_simple_sender/receiver.sh
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/udp_streaming_simple_receiver.sh
+++ remote/udp_streaming_simple_receiver.sh
@@ -1,1 +1,5 @@
+#!/bin/sh
+
+set -x
+
 gst-launch-1.0 udpsrc port=8554 caps=application/x-rtp,media=video,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! autovideosink
--- remote/udp_streaming_simple_sender.sh
+++ remote/udp_streaming_simple_sender.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-HOST=192.168.0.4
+HOST=192.168.0.16
 
 set -x
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 !image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! rtpjpegpay !\
+gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! rtpjpegpay !\
   udpsink host=$HOST port=8554

be421a4274b1dda28aea910071605cc7836db315
Jaeyoung Lee <jylee0121@gmail.com>
2023-08-02 14:10:35 +0900

    update: tcp_streaming_simple_sender.sh
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/tcp_streaming_simple_sender.sh
+++ remote/tcp_streaming_simple_sender.sh
@@ -6,4 +6,4 @@ HOST=127.0.0.1
 
 gst-launch-1.0 -tv v4l2src device=/dev/video0  ! image/jpeg, width=1280, height=720, pixel-aspect-ratio=1/1, framerate=30/1 ! tee name=t \
     t. ! queue ! tcpserversink host=$HOST port=5000 \
-    t. ! queue ! jpegdec ! textoverlay text="local" ! xvimagesink
+    t. ! queue ! jpegdec ! timeoverlay ! textoverlay text="local" ! autovideosink

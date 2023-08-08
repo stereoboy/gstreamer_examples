5b255fe13a5b5188a6cbf50c9714072a2d281aac
Jaeyoung Lee <jylee0121@gmail.com>
2023-08-08 14:14:34 +0900

    update: update tcp_streaming_nvh264_receiver.sh
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/tcp_streaming_nvh264_receiver.sh
+++ remote/tcp_streaming_nvh264_receiver.sh
@@ -4,4 +4,8 @@ set -x
 
 HOST=127.0.0.1
 
+# AGX
 gst-launch-1.0 -tv tcpclientsrc host=$HOST port=5000 ! h264parse ! nvv4l2decoder ! nvvidconv ! autovideosink
+
+# Desktop
+#gst-launch-1.0 -tv tcpclientsrc host=$HOST port=5000 ! h264parse ! nvv4l2decoder ! nvvideoconvert ! autovideosink

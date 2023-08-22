616deef260232662d378115ebfa78be548207705
Jaeyoung Lee <jylee0121@gmail.com>
2023-08-22 18:37:07 +0900

    update: update tcp_streaming_jpeg_sender/receiver.sh
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/tcp_streaming_jpeg_receiver.sh
+++ remote/tcp_streaming_jpeg_receiver.sh
@@ -5,3 +5,10 @@ set -x
 HOST=127.0.0.1
 
 gst-launch-1.0 -tv tcpclientsrc host=$HOST port=5000 ! jpegdec ! autovideosink
+
+#
+# references
+#  - https://stackoverflow.com/questions/50549584/gstreamer-udpsink-udpsrc-versus-tcpserversink-tcpclientsrc
+#  - https://gstreamer.freedesktop.org/documentation/rtp/rtpstreampay.html?gi-language=c#rtpstreampay-page
+#
+#gst-launch-1.0 -tv tcpclientsrc host=$HOST port=5000 ! application/x-rtp-stream,encoding-name=JPEG ! rtpstreamdepay ! rtpjpegdepay ! jpegdec ! autovideosink

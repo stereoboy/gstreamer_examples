b8c3a81ac1c0b6e438ef015ad3b3f7544a204665
Jaeyoung Lee <jylee0121@gmail.com>
2023-08-23 00:27:26 +0900

    update: update gui udp_streaming_simple_sender/receiver for performance test
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/udp_streaming_simple_receiver.sh
+++ remote/udp_streaming_simple_receiver.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-gst-launch-1.0 udpsrc port=8554 caps=application/x-rtp,media=video,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! autovideosink
+gst-launch-1.0 udpsrc port=8554 caps=application/x-rtp,media=video,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! textoverlay text="remote" ! autovideosink
--- remote/udp_streaming_simple_sender.sh
+++ remote/udp_streaming_simple_sender.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-HOST=192.168.0.16
+#HOST=192.168.0.16
+HOST=127.0.0.1
 
 set -x
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! rtpjpegpay !\
-  udpsink host=$HOST port=8554
+gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! tee name=t \
+    t. ! queue ! jpegenc ! rtpjpegpay ! udpsink host=$HOST port=8554 \
+    t. ! queue ! textoverlay text="local" ! autovideosink

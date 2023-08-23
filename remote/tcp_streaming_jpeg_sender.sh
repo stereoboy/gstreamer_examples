c53622196a851405fa2f6f30b8f369dc57b463e3
Jaeyoung Lee <jylee0121@gmail.com>
2023-08-23 19:45:03 +0900

    update: update gui tcp_streaming_jpeg_sender.sh
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/tcp_streaming_jpeg_sender.sh
+++ remote/tcp_streaming_jpeg_sender.sh
@@ -6,8 +6,12 @@ HOST=127.0.0.1
 
 WIDTH=1280
 HEIGHT=720
-
-gst-launch-1.0 -tv v4l2src device=/dev/video0  ! image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! tee name=t \
+#
+# references
+#  - fpsdiplaysink
+#    - https://gstreamer.freedesktop.org/documentation/debugutilsbad/fpsdisplaysink.html?gi-language=c
+#
+gst-launch-1.0 -tv v4l2src device=/dev/video0 ! image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! textoverlay text="${WIDTH}&#215;${HEIGHT}" valignment=top halignment=right ! tee name=t \
     t. ! queue ! jpegenc ! tcpserversink host=$HOST port=5000 \
     t. ! queue ! textoverlay text="local" ! autovideosink
 
@@ -16,6 +20,6 @@ gst-launch-1.0 -tv v4l2src device=/dev/video0  ! image/jpeg, width=${WIDTH}, hei
 #  - https://stackoverflow.com/questions/50549584/gstreamer-udpsink-udpsrc-versus-tcpserversink-tcpclientsrc
 #  - https://gstreamer.freedesktop.org/documentation/rtp/rtpstreampay.html?gi-language=c#rtpstreampay-page
 #
-#gst-launch-1.0 -tv v4l2src device=/dev/video0  ! image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! tee name=t \
-#    t. ! queue ! jpegenc ! rtpjpegpay ! rtpstreampay ! tcpserversink host=$HOST port=5000 \
-#    t. ! queue ! textoverlay text="local" ! autovideosink
+#gst-launch-1.0 -tv v4l2src device=/dev/video0 ! image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! textoverlay text="${WIDTH}&#215;${HEIGHT}" valignment=top halignment=right ! tee name=t \
+#    t. ! queue ! videorate ! jpegenc ! rtpjpegpay ! rtpstreampay ! tcpserversink host=$HOST port=5000 \
+#    t. ! queue ! textoverlay text="local" ! autovideosink \

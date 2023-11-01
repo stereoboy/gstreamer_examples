149fe3c41b60f4260f181dd6f3b729be054dab62
Jaeyoung Lee <jylee0121@gmail.com>
2023-11-01 15:44:20 +0900

    update: add option buffer-size to remove WARN
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/udp_streaming_simple_receiver.sh
+++ remote/udp_streaming_simple_receiver.sh
@@ -2,4 +2,14 @@
 
 set -x
 
-gst-launch-1.0 udpsrc port=8554 caps=application/x-rtp,media=video,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! textoverlay text="remote" ! autovideosink
+PORT=5000
+
+WIDTH=1280
+HEIGHT=720
+
+#
+# to remove "WARN            rtpjpegdepay gstrtpjpegdepay.c:758:gst_rtp_jpeg_depay_process:<rtpjpegdepay0> discarding data packets received when we have no header"
+# references
+#   - https://gstreamer-devel.narkive.com/nud8O26p/possible-bug-in-rtpjpegpay-and-or-rtpjpegdepay
+#
+gst-launch-1.0 -tv udpsrc port=${PORT} buffer-size=$((${WIDTH}*${HEIGHT}*3)) caps=application/x-rtp,media=video,encoding-name=JPEG ! rtpjpegdepay !  queue ! jpegparse ! jpegdec ! textoverlay text="remote" ! autovideosink
--- remote/udp_streaming_simple_sender.sh
+++ remote/udp_streaming_simple_sender.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-#HOST=192.168.0.16
-HOST=127.0.0.1
-
 set -x
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! tee name=t \
-    t. ! queue ! jpegenc ! rtpjpegpay ! udpsink host=$HOST port=8554 \
+HOST=127.0.0.1
+PORT=5000
+
+DEVICE=/dev/video0
+WIDTH=1280
+HEIGHT=720
+FRAMERATE=30/1
+
+gst-launch-1.0 -tv v4l2src device=${DEVICE} ! image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=${FRAMERATE} ! jpegdec ! timeoverlay ! tee name=t \
+    t. ! queue ! jpegenc ! rtpjpegpay ! udpsink host=${HOST} port=${PORT} \
     t. ! queue ! textoverlay text="local" ! autovideosink

16446e65e136543d40c3cd0ca21c6a637759cf20
Jaeyoung Lee <jylee0121@gmail.com>
2023-11-08 19:13:26 +0900

    update: update shell scrips for relay server and delegate
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/relay_with_delegate/tcp_streaming_mixed_delegate_udp.sh
+++ remote/relay_with_delegate/tcp_streaming_mixed_delegate_udp.sh
@@ -2,8 +2,11 @@
 
 set -x
 
+AUDIO_BUFFER_SIZE=100000
+
 #RELAY_HOST=172.31.13.19 # US server
-RELAY_HOST=127.0.0.1
+# RELAY_HOST=127.0.0.1
+RELAY_HOST=192.168.0.2
 RELAY_PORT0=5004
 RELAY_PORT1=5005
 RELAY_PORT2=5006
@@ -18,5 +21,5 @@ PORT3=5011
 gst-launch-1.0 -tv \
     tcpclientsrc host=${RELAY_HOST} port=${RELAY_PORT0} ! application/x-rtp-stream,encoding-name=JPEG ! rtpstreamdepay ! udpsink host=${HOST} port=${PORT0} \
     tcpclientsrc host=${RELAY_HOST} port=${RELAY_PORT1} ! application/x-rtp-stream,encoding-name=JPEG ! rtpstreamdepay ! udpsink host=${HOST} port=${PORT1} \
-    tcpclientsrc host=${RELAY_HOST} port=${RELAY_PORT2} ! application/x-rtp-stream,media=audio, clock-rate=44100, encoding-name=L16, channels=2, channel-mask=0x0000000000000003 ! rtpstreamdepay ! udpsink host=${HOST} port=${PORT2}
-
+    tcpclientsrc host=${RELAY_HOST} port=${RELAY_PORT2} ! application/x-rtp-stream,media=audio, clock-rate=44100, encoding-name=L16, channels=2, channel-mask=0x0000000000000003 ! rtpstreamdepay ! udpsink host=${HOST} port=${PORT2} \
+    # udpsrc  port=${PORT3} buffer-size=${AUDIO_BUFFER_SIZE} ! queue ! rtpstreampay ! tcpclientsink host=${RELAY_HOST} port=${RELAY_PORT3} \
--- remote/relay_with_delegate/udp_streaming_mixed_relay_tcp.sh
+++ remote/relay_with_delegate/udp_streaming_mixed_relay_tcp.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+# SENDER_HOST=127.0.0.1
+SENDER_HOST=192.168.0.13
 PORT0=5000
 PORT1=5001
 PORT2=5002
@@ -13,7 +15,8 @@ HEIGHT=720
 AUDIO_BUFFER_SIZE=100000
 
 #RELAY_HOST=172.31.13.19 # US server
-RELAY_HOST=127.0.0.1
+# RELAY_HOST=127.0.0.1
+RELAY_HOST=192.168.0.2
 RELAY_PORT0=5004
 RELAY_PORT1=5005
 RELAY_PORT2=5006
@@ -22,4 +25,5 @@ RELAY_PORT3=5007
 gst-launch-1.0 -tv \
     udpsrc  port=${PORT0} buffer-size=$((${WIDTH}*${HEIGHT}*3)) ! queue ! rtpstreampay ! tcpserversink host=${RELAY_HOST} port=${RELAY_PORT0} \
     udpsrc  port=${PORT1} buffer-size=$((${WIDTH}*${HEIGHT}*3)) ! queue ! rtpstreampay ! tcpserversink host=${RELAY_HOST} port=${RELAY_PORT1} \
-    udpsrc  port=${PORT2} buffer-size=${AUDIO_BUFFER_SIZE} ! queue ! rtpstreampay ! tcpserversink host=${RELAY_HOST} port=${RELAY_PORT2}
+    udpsrc  port=${PORT2} buffer-size=${AUDIO_BUFFER_SIZE} ! queue ! rtpstreampay ! tcpserversink host=${RELAY_HOST} port=${RELAY_PORT2} \
+    # tcpserversrc host=${RELAY_HOST} port=${RELAY_PORT3} ! application/x-rtp-stream,media=audio, clock-rate=44100, encoding-name=L16, channels=2, channel-mask=0x0000000000000003 ! rtpstreamdepay ! udpsink host=${SENDER_HOST} port=${PORT3}

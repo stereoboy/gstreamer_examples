09450e6de9ba48079ae51befca2a654b4572831c
Jaeyoung Lee <jylee0121@gmail.com>
2024-01-09 15:38:39 +0900

    update: update udp_streaming_mux_sender/receiver.sh (not working)
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- remote/udp_streaming_mux_receiver.sh
+++ remote/udp_streaming_mux_receiver.sh
@@ -2,10 +2,16 @@
 
 #gst-launch-1.0 udpsrc port=8554 ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
 
+PORT=5000
 
+WIDTH=640
+HEIGHT=480
 
-gst-launch-1.0 -v  \
-  udpsrc port=8554 caps=application/x-rtp ! rtpptdemux name=demux \
-   demux. ! application/x-rtp,encoding-name=JPEG,media=video,clock-rate=90000,payload=26 ! queue ! rtpjpegdepay ! jpegdec ! autovideosink \
-   demux. ! application/x-rtp,encoding-name=JPEG,media=video,clock-rate=90000,payload=96 ! queue ! rtpjpegdepay ! jpegdec ! autovideosink \
+gst-launch-1.0 -tv -e \
+    udpsrc port=${PORT} buffer-size=$((${WIDTH}*${HEIGHT})) ! \
+    application/x-rtp,encoding-name=JPEG,media=video,clock-rate=90000 ! \
+    rtpjitterbuffer latency=200 ! rtpptdemux name=demux \
+    demux. ! application/x-rtp,encoding-name=JPEG,media=video,clock-rate=90000,payload=26 ! queue ! rtpjpegdepay ! jpegparse ! jpegdec ! textoverlay text="remote left" ! autovideosink \
+    demux. ! application/x-rtp,encoding-name=JPEG,media=video,clock-rate=90000,payload=96 ! queue ! rtpjpegdepay ! jpegparse ! jpegdec ! textoverlay text="remote right" ! autovideosink \
+
 
--- remote/udp_streaming_mux_sender.sh
+++ remote/udp_streaming_mux_sender.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-HOST=192.168.0.4
-
+#HOST=192.168.0.4
+HOST=192.168.55.75
+PORT=5000
 # baseline
 #gst-launch-1.0 -v v4l2src device=/dev/video0 !image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! rtpjpegpay !\
 #  udpsink host=127.0.0.1 port=8554
 
+DEVICE0=/dev/video4
+DEVICE1=/dev/video6
 
-gst-launch-1.0  rtpmux name=mux ! udpsink host=$HOST port=8553 \
-  v4l2src device=/dev/video0 !image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! rtpjpegpay ! application/x-rtp, payload=26 ! mux.sink_0 \
-  v4l2src device=/dev/video1 !image/jpeg, width=640, height=480, pixel-aspect-ratio=1/1, framerate=30/1 ! rtpjpegpay ! application/x-rtp, payload=96 ! mux.sink_1 \
+WIDTH=640
+HEIGHT=480
+
+gst-launch-1.0 -tv -e \
+  rtpmux name=mux ! udpsink host=${HOST} port=${PORT} \
+  v4l2src device=${DEVICE0} !image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! tee name=t0 \
+  t0. ! queue ! jpegenc ! image/jpeg, framerate=30/1 ! rtpjpegpay ! application/x-rtp, payload=26 ! mux.sink_0 \
+  t0. ! queue ! textoverlay text="local left" ! autovideosink \
+  v4l2src device=${DEVICE1} !image/jpeg, width=${WIDTH}, height=${HEIGHT}, pixel-aspect-ratio=1/1, framerate=30/1 ! jpegdec ! timeoverlay ! tee name=t1 \
+  t1. ! queue ! jpegenc ! image/jpeg, framerate=30/1 ! rtpjpegpay ! application/x-rtp, payload=96 ! mux.sink_1 \
+  t1. ! queue ! textoverlay text="local right" ! autovideosink \
+
 
